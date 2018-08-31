ed8c1357597b2a74ed53b86dc8c47110abf6d25a
Luisetti Jeremy <jeyremy@cap6.mobi>
2018-08-31 12:05:58 +0200

    OK added on / off with dummy volume 0 to default (conf)

--- index.js
+++ index.js
@@ -22,44 +22,43 @@ function ReceiverVolume(log, config) {
     this.defaultVolume = config['defaultVolume'] || 35; // prevent for 100% volume
     this.host = confHost  = config['host'];
     this.lastVolume = 0;
-    
+
     //cap maxVolume.  Devialet percentage maxes at 98 in receiver settings
     if(this.maxVolume > 98)
         this.maxVolume = 98;
-    
+
     if (!this.host) {
         this.log.warn('Config is missing host/IP of receiver');
         callback(new Error('No host/IP defined.'));
         return;
     }
-    
+
     searchSpeaker();
 
-    
 }
 
 
 client.on('response', function inResponse(headers, code, rinfo) {
-          
-          //  console.log("Found something at "+rinfo.address+"===?"+confHost);
-          
-          if(rinfo.address == confHost)
-          {
-              actualDevice["host"] = parseUri(headers.LOCATION).host;
-              actualDevice["port"] = parseUri(headers.LOCATION).port;
-              logBridge.info("Devialet Bridge found at "+actualDevice["host"]+":"+actualDevice["port"]);
-          }
- })
+
+    //  console.log("Found something at "+rinfo.address+"===?"+confHost);
+
+    if(rinfo.address == confHost)
+    {
+        actualDevice["host"] = parseUri(headers.LOCATION).host;
+        actualDevice["port"] = parseUri(headers.LOCATION).port;
+        logBridge.info("Devialet Bridge found at "+actualDevice["host"]+":"+actualDevice["port"]);
+    }
+})
 
 function searchSpeaker()
 {
     logBridge.info("Seraching for Devialet bridge");
     client.search('urn:schemas-upnp-org:service:RenderingControl:2');
-    
+
     setTimeout(function() {
-               if(!actualDevice["host"]) searchSpeaker();
-               }, discoverInterval);
-    
+        if(!actualDevice["host"]) searchSpeaker();
+    }, discoverInterval);
+
     if(actualDevice["host"])
     {
         logBridge.info("Devialet bridge found at: "+actualDevice["host"]+":"+actualDevice["port"]);
@@ -69,20 +68,20 @@ function searchSpeaker()
 ReceiverVolume.prototype.getStatus = function(callback) {
     var statusUrl = `http://${this.host}/goform/form${this.zoneName}_${this.zoneName}XmlStatus.xml`;
     request.get(statusUrl, function (error, response, body) {
-                var xml = '';
-                if (!error && response.statusCode == 200) {
-                parseString(xml + body, function (err, result) {
-                            callback(result.item);
-                            }.bind(this));
-                }else{
-                  reSearchPort();
-                  callback(null);
-                }
-                }.bind(this));
+        var xml = '';
+        if (!error && response.statusCode == 200) {
+            parseString(xml + body, function (err, result) {
+                callback(result.item);
+            }.bind(this));
+        }else{
+            reSearchPort();
+            callback(null);
+        }
+    }.bind(this));
 }
 
 ReceiverVolume.prototype.setControl = function (control, val, callback) {
-    
+
     if(!actualDevice["host"])
     {
         var status =  "Sorry: Speakers not found yet. Try later..";
@@ -90,46 +89,46 @@ ReceiverVolume.prototype.setControl = function (control, val, callback) {
     else
     {
         var status = "Set Volume to: "+val+"%";
-        
+
         var xml = '<s:Envelope s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">'+
-        '<s:Body>'+
-        '<u:SetVolume xmlns:u="urn:schemas-upnp-org:service:RenderingControl:2">'+
-        '<InstanceID>0</InstanceID>'+
-        '<Channel>Master</Channel>'+
-        '<DesiredVolume>'+val+'</DesiredVolume>'+
-        '</u:SetVolume>'+
-        '</s:Body>'+
-        '</s:Envelope>';
-        
+            '<s:Body>'+
+            '<u:SetVolume xmlns:u="urn:schemas-upnp-org:service:RenderingControl:2">'+
+            '<InstanceID>0</InstanceID>'+
+            '<Channel>Master</Channel>'+
+            '<DesiredVolume>'+val+'</DesiredVolume>'+
+            '</u:SetVolume>'+
+            '</s:Body>'+
+            '</s:Envelope>';
+
         var http_options = {
-        hostname: actualDevice["host"],
-        port: actualDevice["port"], //a vérifier car pas sur.
-        path: '/Control/LibRygelRenderer/RygelRenderingControl',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'SOAPACTION': 'urn:schemas-upnp-org:service:RenderingControl:2#SetVolume',
-            'Content-Length': xml.length
+            hostname: actualDevice["host"],
+            port: actualDevice["port"], //a vérifier car pas sur.
+            path: '/Control/LibRygelRenderer/RygelRenderingControl',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'SOAPACTION': 'urn:schemas-upnp-org:service:RenderingControl:2#SetVolume',
+                'Content-Length': xml.length
+            }
         }
-        }
-        
+
         var req = http.request(http_options, (res) => {
-           res.setEncoding('utf8');
-                if ( res.statusCode == 200) {
-                   this.lastVolume = val;
-                   callback(null);
-               } else {
-                   reSearchPort();
-                   callback(res.statusCode);
-               }
-           });
-        req.on('error', (e) => {
-                this.log.warn(`problem with request: ${e.message}`);
+            res.setEncoding('utf8');
+            if ( res.statusCode == 200) {
+                this.lastVolume = val;
+                callback(null);
+            } else {
                 reSearchPort();
+                callback(res.statusCode);
+            }
+        });
+        req.on('error', (e) => {
+            this.log.warn(`problem with request: ${e.message}`);
+            reSearchPort();
 
-                callback(e);
+            callback(e);
 
-         });
+        });
 
         req.write(xml);
         req.end();
@@ -143,7 +142,6 @@ ReceiverVolume.prototype.setBrightness = function(newLevel, callback) {
     if(newLevel>this.maxVolume)newLevel = this.maxVolume;
     this.setControl('Volume', newLevel, callback);
 }
-
 ReceiverVolume.prototype.setPowerStateOn = function(callback) {
     newLevel = this.defaultVolume;
     this.setControl('Volume', newLevel, callback);
@@ -152,6 +150,7 @@ ReceiverVolume.prototype.setPowerStateOff = function(callback) {
     newLevel = 0;
     this.setControl('Volume', newLevel, callback);
 }
+
 function reSearchPort(){
     actualDevice["host"] = null;
     actualDevice["port"] = null;
@@ -160,15 +159,15 @@ function reSearchPort(){
 
 ReceiverVolume.prototype.getBrightness = function(callback) {
 
-     var status ='';
+    var status ='';
 
     if(!actualDevice["host"])
     {
-          status =  "Sorry: Speakers not found yet. Try later..";
+        status =  "Sorry: Speakers not found yet. Try later..";
     }
     else
     {
-          status = "Get Volume.";
+        status = "Get Volume.";
 
         var xml = '<s:Envelope s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">'+
             '<s:Body>'+
@@ -209,7 +208,7 @@ ReceiverVolume.prototype.getBrightness = function(callback) {
                     var from = data.indexOf("<CurrentVolume>") +15;
                     var to = data.indexOf("</CurrentVolume>") ;
                     var currentVolume  =  data.substr(from,to-from);
-                   // this.log.info(currentVolume);
+                    // this.log.info(currentVolume);
                     callback(null,currentVolume);
                 }
 
@@ -219,11 +218,11 @@ ReceiverVolume.prototype.getBrightness = function(callback) {
                 callback(e, this.lastVolume);
             });
         });
-       req.on('error', (e) => {
-           this.log.warn(`problem with request: ${e.message}`);
-           reSearchPort();
-           callback(e, this.lastVolume);
-          //  callback(e);
+        req.on('error', (e) => {
+            this.log.warn(`problem with request: ${e.message}`);
+            reSearchPort();
+            callback(e, this.lastVolume);
+            //  callback(e);
         });
         req.write(xml);
         req.end();
@@ -234,12 +233,11 @@ ReceiverVolume.prototype.getBrightness = function(callback) {
 
 ReceiverVolume.prototype.getServices = function() {
     var lightbulbService = new Service.Lightbulb(this.name);
-    
-    lightbulbService
-    .addCharacteristic(new Characteristic.Brightness())
-    .on('set', this.setBrightness.bind(this))
-    .on('get', this.getBrightness.bind(this));
 
+    lightbulbService
+        .addCharacteristic(new Characteristic.Brightness())
+        .on('set', this.setBrightness.bind(this))
+        .on('get', this.getBrightness.bind(this));
 
     lightbulbService
         .getCharacteristic(Characteristic.On)
@@ -256,17 +254,17 @@ ReceiverVolume.prototype.getServices = function() {
 
 function parseUri (str) {
     var    o   = parseUri.options,
-    m   = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
-    uri = {},
-    i   = 14;
-    
+        m   = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
+        uri = {},
+        i   = 14;
+
     while (i--) uri[o.key[i]] = m[i] || "";
-    
+
     uri[o.q.name] = {};
     uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
-                           if ($1) uri[o.q.name][$1] = $2;
-                           });
-    
+        if ($1) uri[o.q.name][$1] = $2;
+    });
+
     return uri;
 };
 
